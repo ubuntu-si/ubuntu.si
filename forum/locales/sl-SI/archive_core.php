315a525d03b71599c4af055c0db28e52cd3d437e
CrazyLemon <drazen@ubuntu.si>
2014-05-19 00:11:15 +0200

    Design and translation files for forum 2.0

---
+++ forum/locales/sl-SI/archive_core.php
@@ -0,0 +1,7 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$Definition['GenderSuffix.First.f'] = 'GenderSuffix.First.f';
+$Definition['GenderSuffix.First.m'] = 'GenderSuffix.First.m';
+$Definition['GenderSuffix.Third.f'] = 'GenderSuffix.Third.f';
+$Definition['GenderSuffix.Third.m'] = 'GenderSuffix.Third.m';
+$Definition['GenderSuffix.Third.u'] = 'GenderSuffix.Third.u';
---
+++ forum/locales/sl-SI/definitions.php
@@ -0,0 +1,20 @@
+<?php if (!defined('APPLICATION')) exit();
+/*
+Copyright 2008, 2009 Vanilla Forums Inc.
+This file is part of Garden.
+Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
+Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
+*/
+
+$LocaleInfo['sl-SI'] = array( // make sure the key of this array is the same as its folder name.
+   'Locale'       => 'sl-SI',
+   'Name'         => 'Slovenian Locale',
+   'Description'  => 'This is vanilla locale in Slovenian.',
+   'Version'      => '0.1',
+   'Author'       => "Dražen M",
+   'AuthorEmail'  => 'drazen@ubuntu.si',
+   'AuthorUrl'    => 'https://ubuntu.si',
+   'License'      => 'MIT'
+);
