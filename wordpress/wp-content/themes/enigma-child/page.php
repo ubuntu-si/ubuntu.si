af1518d286a7aa51a27628f9f2b06a83597dd5e6
CrazyLemon <drazen@ubuntu.si>
2015-05-23 21:42:21 +0200

    WP: Too many divs with the same name; .container is now known as .enigma_container

--- wordpress/wp-content/themes/enigma-child/page.php
+++ wordpress/wp-content/themes/enigma-child/page.php
@@ -1,6 +1,6 @@
 <?php //Template Name:Full-Width Page
 get_header(); ?>
-<div class="container">
+<div class="enigma_container">
 	<div class="row enigma_blog_wrapper">
 	<div class="col-md-12">
 	<?php get_template_part('post','page'); ?>	
