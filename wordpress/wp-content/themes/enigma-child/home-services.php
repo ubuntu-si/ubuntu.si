7c09f712717668d1b9d157117fd73ba283e2268d
CrazyLemon <drazen@ubuntu.si>
2015-05-25 13:04:54 +0200

    Updating our-services

--- wordpress/wp-content/themes/enigma-child/home-services.php
+++ wordpress/wp-content/themes/enigma-child/home-services.php
@@ -18,11 +18,11 @@
               <i class="fa fa-rss"></i>
           </div>
           <div class="wordpress_feed enigma_service_detail media-body">
-            <h3>Zadnje novice</h3>
+            <h3>Zadnje s foruma</h3>
             <?php // Get RSS Feed(s)
             include_once( ABSPATH . WPINC . '/feed.php' );
             // Get a SimplePie feed object from the specified feed source.
-            $rss = fetch_feed( 'https://www.ubuntu.si/feed/' );
+            $rss = fetch_feed( 'https://www.ubuntu.si/forum/discussions/all/feed.rss' );
             $maxitems = 0;
             if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
               // Figure out how many total items there are, but limit it to 5.
@@ -38,7 +38,7 @@
                 <?php // Loop through each feed item and display each item as a hyperlink. ?>
                 <?php foreach ( $rss_items as $item ) : ?>
                   <li>
-                    <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
+                    <a target="_blank" href="<?php echo esc_url( $item->get_permalink() ); ?>"
                       title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
                       <?php echo esc_html( $item->get_title() ); ?>
                     </a>
@@ -52,43 +52,16 @@
       <div class="col-md-4 service">
         <div class="enigma_service_area appear-animation bounceIn appear-animation-visible">
           <div class="enigma_service_iocn pull-left">
-              <i class="fa fa-rss"></i>
+              <i class="fa fa-cloud-download"></i>
           </div>
           <div class="forum_feed enigma_service_detail media-body">
-            <h3>Zadnja sporočila s foruma</h3>
-            <?php // Get RSS Feed(s)
-            include_once( ABSPATH . WPINC . '/feed.php' );
-
-            // Get a SimplePie feed object from the specified feed source.
-            $rss = fetch_feed( 'https://www.ubuntu.si/forum/discussions/all/feed.rss' );
-
-            $maxitems = 0;
-
-            if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
-
-              // Figure out how many total items there are, but limit it to 5.
-              $maxitems = $rss->get_item_quantity( 5 );
-
-              // Build an array of all the items, starting with element 0 (first element).
-              $rss_items = $rss->get_items( 0, $maxitems );
-
-            endif;
-            ?>
-
+            <h3>Priporočamo prejem LTS različic</h3>
             <ul>
-              <?php if ( $maxitems == 0 ) : ?>
-                <li><?php _e( 'No items', 'my-text-domain' ); ?></li>
-              <?php else : ?>
-                <?php // Loop through each feed item and display each item as a hyperlink. ?>
-                <?php foreach ( $rss_items as $item ) : ?>
-                  <li>
-                    <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
-                      title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
-                      <?php echo esc_html( $item->get_title() ); ?>
-                    </a>
-                  </li>
-                <?php endforeach; ?>
-              <?php endif; ?>
+              <li><a target="_blank" href="http://releases.ubuntu.com/14.04/ubuntu-14.04.2-desktop-amd64.iso.torrent">Ubuntu 14.04 LTS</a></li>
+              <li><a target="_blank" href="http://releases.ubuntu.com/14.04/kubuntu-14.04.2-desktop-amd64.iso.torrent">Kubuntu 14.04 LTS</a></li>
+              <li><a target="_blank" href="http://releases.ubuntu.com/14.04/xubuntu-14.04.2-desktop-amd64.iso.torrent">Xubuntu 14.04 LTS</a></li>
+              <li><a target="_blank" href="http://releases.ubuntu.com/14.04/ubuntu-gnome-14.04.2-desktop-amd64.iso.torrent">Ubuntu GNOME 14.04 LTS</a></li>
+              <li><a target="_blank" href="https://ubuntu-mate.org/trusty/ubuntu-mate-14.04.2-LTS-desktop-amd64.iso.torrent">Ubuntu MATE 14.04 LTS</a></li>
             </ul>
           </div>
         </div>
@@ -101,10 +74,10 @@
           <div class="other_links enigma_service_detail media-body">
           <h3>Druge zanimive povezave</h3>
           <ul>
-            <li>Prva povezava (ogled)</li>
-            <li>Druga povezava (predstavitev)</li>
-            <li>Tretja povezava nekaj tretjega? PDF?</li>
-            <li>Povezava do youtubea?</li>
+            <li><a target="_blank" href="https://www.ubuntu.si/ogled/sl/">Oglejte si kako zgleda Ubuntu</a></li>
+            <li><a target="_blank" href="https://www.ubuntu.si/predstavitev/">Predstavitev Ubuntu</a></li>
+            <li><a target="_blank" href="http://matej.owca.info/linux/Linux_na_namizju.pdf">PDF knjiga Linux na namizju</a></li>
+            <li><a target="_blank" href="//www.ubuntu.si/povezave/">...</a></li>
           </ul>
           </div>
         </div>
