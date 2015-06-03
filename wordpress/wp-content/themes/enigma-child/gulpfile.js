5af1c5c9828c5b57c5be06f00697be710ea76e63
CrazyLemon <drazen@ubuntu.si>
2015-06-03 17:16:15 +0200

    WP/enigma-child: gulp file finished and working - when ran manually. dz0ny automate this thing. ktnx

--- wordpress/wp-content/themes/enigma-child/gulpfile.js
+++ wordpress/wp-content/themes/enigma-child/gulpfile.js
@@ -1,8 +1,14 @@
+var gulp = require('gulp');
 var concat = require('gulp-concat');
+watch = require('gulp-watch');
 
 gulp.task('css', function () {
     gulp.src('css/*.css')
         .pipe(concat('style.css'))
         .pipe(gulp.dest('./'));
 });
-gulp.task('default', ['css']);
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('css/*.css', ['css']);
+});
+gulp.task('default', ['watch', 'css']);
