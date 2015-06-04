ffeb92f37c778006397ceb9c4afe45c6f3ab5a5d
R33D3M33R <R33D3M33R@users.noreply.github.com>
2015-06-04 13:40:46 +0200

    Add custom page for pogosta vprašanja

---
+++ wordpress/wp-content/themes/enigma-child/page-pogosta_vprasanja.php
@@ -0,0 +1,11 @@
+<?php //Template Name:Full-Width Page
+get_header(); ?>
+<div class="enigma_container">
+	<div class="row enigma_blog_wrapper">
+	<div class="col-md-12">
+	<h2>Katero različico Ubuntuja naj namestim?</h2>
+	<?php get_template_part('post','page'); ?>
+	</div>
+	</div>
+</div>	
+<?php get_footer(); ?>
