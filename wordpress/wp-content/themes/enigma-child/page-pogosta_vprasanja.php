43bf1835be1b6beccbb9df41c8ad2d072fcf17ca
R33D3M33R <R33D3M33R@users.noreply.github.com>
2015-06-04 13:53:24 +0200

    Add intro and wrapper

--- wordpress/wp-content/themes/enigma-child/page-pogosta_vprasanja.php
+++ wordpress/wp-content/themes/enigma-child/page-pogosta_vprasanja.php
@@ -3,7 +3,15 @@ get_header(); ?>
 <div class="enigma_container">
 	<div class="row enigma_blog_wrapper">
 	<div class="col-md-12">
-	<h2>Katero različico Ubuntuja naj namestim?</h2>
+		<div class="enigma_blog_full">
+			<div class="enigma_blog_post_content">
+				<h2>Katero različico Ubuntuja naj namestim?</h2>
+				<p>Ubuntu obstaja v večih različicah, ki so prilagojene za določeno strojno opremo, ponujajo različne videze in
+					privzeto nameščene programe, so v koraku s časom ali pa bolj stabilne, itd. Izbira je vedno dobrodošla, vendar lahko zmede uporabnike, ki niso tehnološko podkovani, zato smo pripravili kratek vprašalnik, ki vam bo pomagal izbrati pravo različico Ubuntu-ja.</p>
+				<div id="ubuntu_vprasalnik">
+				</div>
+			</div>
+		</div>
 	<?php get_template_part('post','page'); ?>
 	</div>
 	</div>
