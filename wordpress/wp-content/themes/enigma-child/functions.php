d7837a0409493ae7548971345dc95d6aa3393905
CrazyLemon <drazen@matesic.si>
2016-05-14 23:39:30 +0200

    WP: Removing sh1t and animations

--- wordpress/wp-content/themes/enigma-child/functions.php
+++ wordpress/wp-content/themes/enigma-child/functions.php
@@ -7,29 +7,26 @@ function theme_enqueue_custom()
     wp_enqueue_script('menu', WL_TEMPLATE_DIR_URI .'/js/menu.js', array('jquery'), false, true);
     wp_enqueue_script('bootstrap-js', WL_TEMPLATE_DIR_URI .'/js/bootstrap.js', array('jquery'), false, true);
     wp_enqueue_script('enigma-theme-script', WL_TEMPLATE_DIR_URI .'/js/enigma_theme_script.js', array('jquery'), false, true);
-    if(is_front_page()){
-        wp_enqueue_script('jquery.carouFredSel', WL_TEMPLATE_DIR_URI .'/js/carouFredSel-6.2.1/jquery.carouFredSel-6.2.1.js', array('jquery'), false, true);
-        wp_enqueue_script('carouFredSel-element', WL_TEMPLATE_DIR_URI .'/js/carouFredSel-6.2.1/caroufredsel-element.js', array('jquery'), false, true);
-        wp_enqueue_script('photobox-js', WL_TEMPLATE_DIR_URI .'/js/jquery.photobox.js', array('jquery'), false, true);
-    }
 }
 
 function theme_dequeue_custom()
 {
     wp_dequeue_style('OpenSansRegular');
-    wp_dequeue_style('OpenSansBold');
     wp_dequeue_style('OpenSans');
+    wp_dequeue_style('OpenSansBold');
     wp_dequeue_style('OpenSansSemiBold');
     wp_dequeue_style('RobotoRegular');
     wp_dequeue_style('RobotoBold');
     wp_dequeue_style('RalewaySemiBold');
     wp_dequeue_style('Courgette');
+    wp_deregister_style( 'photobox' );
     wp_dequeue_script('menu');
     wp_dequeue_script('bootstrap-js');
     wp_dequeue_script('enigma-theme-script');
     wp_dequeue_script('jquery.carouFredSel');
     wp_dequeue_script('carouFredSel-element');
-    /* wp_dequeue_script('photobox-js'); - cos this sh1t is giving us errors man errors!*/
+    wp_dequeue_script('photobox-js');
+    wp_dequeue_script('enigma-footer-script');
 }
 
 function wp2vanilla_post_comments()
